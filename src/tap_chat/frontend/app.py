01af4e135fec873f27e1d5a912057d32198f43d4
toi-dawn <toi@dawnhealth.com>
2025-04-05 11:30:52 +0200

    frontend to backend url

--- src/tap_chat/frontend/app.py
+++ src/tap_chat/frontend/app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import httpx
 
+FASTAPI_URL = "https://tap-chat-backend-cfcaaxdkcqcsapf4.westeurope-01.azurewebsites.net/"
+
 st.title("FastAPI + Streamlit Chatbot 🤖")
 
 user_input = st.text_input("You:", "")
 
 if st.button("Send"):
     if user_input:
-        response = httpx.post("http://localhost:8000/chat", json={"user_message": user_input})
+        response = httpx.post(f"{FASTAPI_URL}/chat", json={"user_message": user_input})
         bot_reply = response.json()["response"]
         st.text_area("Bot:", value=bot_reply, height=100)
